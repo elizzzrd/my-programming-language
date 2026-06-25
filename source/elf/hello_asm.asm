985302a0d8840c0625833570639bdfe76a4552b2
Elizaveta Gardina <lizigard@gmail.com>
2026-06-25 22:42:49 +0300

    first working elf!

---
+++ source/elf/hello_asm.asm
@@ -0,0 +1,16 @@
+global _start
+
+section .data
+message: db 'Hello!', 10
+
+section .text
+_start:
+    mov rax, 1          ; 1 == write syscall
+    mov rdi, 1          ; file descriptor = stdout = 1
+    mov rsi, message    ; address of bytes to write
+    mov rdx, 7          ; how many bytes
+    syscall
+
+    mov rax, 60         ; 60 == exit syscall
+    xor rdi, rdi        
+    syscall
